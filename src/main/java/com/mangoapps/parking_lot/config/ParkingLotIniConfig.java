fab84041bddfe7e782b5effb3f6e30cbe0523e1e
rushabhpahade <rushabhpahade98@gmail.com>
2023-10-29 18:27:53 +0530

    Issue7: Create Parking Lot when Application Starts

---
+++ src/main/java/com/mangoapps/parking_lot/config/ParkingLotIniConfig.java
@@ -0,0 +1,31 @@
+package com.mangoapps.parking_lot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.mangoapps.parking_lot.model.ParkingSpot;
+import com.mangoapps.parking_lot.repository.ParkingSpotRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class ParkingLotIniConfig {
+	@Autowired
+    private ParkingSpotRepository parkingSpotRepository;
+    
+	//Set the Intial Size of Parking Lot as 5.
+    private int defaultSize=5;;
+
+    @PostConstruct
+    public void initializeParkingLot() {
+        parkingSpotRepository.deleteAll();
+
+        // Create the parking spots
+        for (int i = 0; i < defaultSize; i++) {
+            ParkingSpot spot = new ParkingSpot();
+            spot.setOccupied(false);
+            parkingSpotRepository.save(spot);
+        }
+    }
+}
